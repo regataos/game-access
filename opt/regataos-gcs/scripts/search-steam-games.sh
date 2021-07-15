81eaf2ba5619568f2966de144d947e710243a6b4
Regata OS Project <regataosproject@gmail.com>
2021-07-14 23:40:55 -0300

    A few more minor improvements to Steam support.

--- opt/regataos-gcs/scripts/search-steam-games.sh
+++ opt/regataos-gcs/scripts/search-steam-games.sh
@@ -34,10 +34,19 @@ cat > "/tmp/regataos-gcs/config/steam-games-json/$game_appid.json" << STEAMGAMEJ
 STEAMGAMEJSON
 }
 
+# Check if the Steam game is still installed
+for i in /tmp/regataos-gcs/config/steam-games-json/*.json; do
+	file_id=$(echo $i | cut -d"/" -f 6- | cut -d"." -f -1)
+
+	if ! test -e "$HOME/.local/share/Steam/steamapps/$file_id.acf"; then
+		rm -f "/tmp/regataos-gcs/config/steam-games-json/$file_id.json"
+	fi
+done
+
 # Get game information and check if they are installed
 for i in $HOME/.local/share/Steam/steamapps/*.acf; do
 	game_appid="$(grep -R '"appid"' $i | awk '{print $2}' | sed 's/"\|,//g')"
-	game_name="$(grep -R '"name"' $i | awk '{print $2}' | sed 's/"\|,//g')"
+	game_name="$(grep -R '"name"' $i | cut -d'"' -f 4- | cut -d'"' -f -1)"
 
 	if [ ! -z $game_appid ];then
 		if test ! -e "/tmp/regataos-gcs/config/steam-games-json/$game_appid.json"; then
