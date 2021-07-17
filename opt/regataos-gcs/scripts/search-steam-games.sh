0e512cea6c41040654504a5ca552742995c8bd8a
Regata OS Project <regataosproject@gmail.com>
2021-07-16 23:35:20 -0300

    More Steam support improvements.

--- opt/regataos-gcs/scripts/search-steam-games.sh
+++ opt/regataos-gcs/scripts/search-steam-games.sh
@@ -17,12 +17,12 @@ if test ! -e "/tmp/regataos-gcs/config/steam-games.conf"; then
 	echo "" > "/tmp/regataos-gcs/config/steam-games.conf"
 fi
 
-if test ! -e "/tmp/regataos-gcs/config/steam-games/json"; then
-	mkdir -p "/tmp/regataos-gcs/config/steam-games/json"
+if test ! -e "/tmp/regataos-gcs/config/steam-games/json/installed"; then
+	mkdir -p "/tmp/regataos-gcs/config/steam-games/json/installed"
 fi
 
-if test ! -e "/tmp/regataos-gcs/config/steam-games/img"; then
-	mkdir -p "/tmp/regataos-gcs/config/steam-games/img"
+if test ! -e "/tmp/regataos-gcs/config/steam-games/json/steam-id"; then
+	mkdir -p "/tmp/regataos-gcs/config/steam-games/json/steam-id"
 fi
 
 # Check if there are any Steam games installed
@@ -34,41 +34,32 @@ if [[ $(ls $HOME/.local/share/Steam/steamapps/ | grep acf) == *"acf"* ]]; then
 else
 	if test ! -e "/tmp/regataos-gcs/config/steam-games/no-steam-games.txt" ;then
 		echo "No Steam games" > "/tmp/regataos-gcs/config/steam-games/no-steam-games.txt"
-		rm -f /tmp/regataos-gcs/config/steam-games/img/*
-		rm -f /tmp/regataos-gcs/config/steam-games/json/*
+		rm -f /tmp/regataos-gcs/config/steam-games/json/installed/*
+		rm -f /tmp/regataos-gcs/config/steam-games/json/steam-id/*
 	fi
 fi
 
 # Check if the game installation directory exists, looking for the game executable
 function search_steam_games() {
 #Create JSON file
-cat > "/tmp/regataos-gcs/config/steam-games/json/$game_appid.json" << STEAMGAMEJSON
+cat > "/tmp/regataos-gcs/config/steam-games/json/installed/$game_appid.json" << STEAMGAMEJSON
 [
 	{
 		"appid": "$game_appid",
 		"name": "$game_name",
-		"launcher": "Steam",
-		"image": "file:///tmp/regataos-gcs/config/steam-games/img/$game_appid.jpg"
     }
 ]
 STEAMGAMEJSON
-
-#Download game image
-if test ! -e "/tmp/regataos-gcs/config/steam-games/img/$game_appid.jpg"; then
-	wget --no-check-certificate -O "/tmp/regataos-gcs/config/steam-games/img/$game_appid.jpg" \
-	"https://cdn.cloudflare.steamstatic.com/steam/apps/$game_appid/header.jpg"
-fi
 }
 
 # Run game processes only if necessary.
 if test ! -e "/tmp/regataos-gcs/config/steam-games/no-steam-games.txt" ;then
 	# Check if the Steam game is still installed
-	for i in /tmp/regataos-gcs/config/steam-games/json/*.json; do
+	for i in /tmp/regataos-gcs/config/steam-games/json/installed/*.json; do
 		file_id=$(echo $i | cut -d"/" -f 6- | cut -d"." -f -1)
 
 		if ! test -e "$HOME/.local/share/Steam/steamapps/$file_id.acf"; then
-			rm -f "/tmp/regataos-gcs/config/steam-games/json/$file_id.json"
-			rm -f "/tmp/regataos-gcs/config/steam-games/img/$game_appid.jpg"
+			rm -f "/tmp/regataos-gcs/config/steam-games/json/installed/$file_id.json"
 		fi
 	done
 
@@ -78,9 +69,23 @@ if test ! -e "/tmp/regataos-gcs/config/steam-games/no-steam-games.txt" ;then
 		game_name="$(grep -R '"name"' $i | cut -d'"' -f 4- | cut -d'"' -f -1)"
 
 		if [ ! -z $game_appid ];then
-			if test ! -e "/tmp/regataos-gcs/config/steam-games/json/$game_appid.json"; then
+			if test ! -e "/tmp/regataos-gcs/config/steam-games/json/installed/$game_appid.json"; then
 				search_steam_games
 			fi
 		fi
 	done
+
+	# View all available games in the user's Steam account
+	if test -e "$HOME/.local/share/Steam/config/config.vdf"; then
+
+		# Download the json file with game information
+		echo "$(grep -r SteamID $HOME/.local/share/Steam/config/config.vdf | awk '{print $2}' | sed 's/"//g')" > "/tmp/regataos-gcs/config/steam-games/json/steam-id/all-steam-id.txt"
+
+		while IFS= read -r steam_games || [[ -n "$steam_games" ]]; do
+			if test ! -e "/tmp/regataos-gcs/config/steam-games/json/steam-id/$steam_games.json"; then
+    			wget --no-check-certificate -O "/tmp/regataos-gcs/config/steam-games/json/steam-id/$steam_games.json" \
+				"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=F06912D875764FDC65C1880539873AE0&steamid=$steam_games&format=json&include_appinfo=true"
+			fi
+		done < "/tmp/regataos-gcs/config/steam-games/json/steam-id/all-steam-id.txt"
+	fi
 fi
--- opt/regataos-gcs/www/js/js-pages/steam-games-list.js
+++ opt/regataos-gcs/www/js/js-pages/steam-games-list.js
@@ -1,12 +1,68 @@
 // This script helps to dynamically create game blocks
+function list_steam_games() {
+	var fs = require("fs");
+
+	var files = [];
+
+	// Read JSON files with the list of games
+	fs.readdirSync("/tmp/regataos-gcs/config/steam-games/json/steam-id").forEach(files => {
+	fs.readFile("/tmp/regataos-gcs/config/steam-games/json/steam-id/" + files, "utf8", function(err, data) {
+	if(!err) {
+		var steam_games = JSON.parse(data);
+		steam_games = steam_games.response.games;
+
+		// Request the dynamic creation of game blocks on the HTML page
+		//Capture the main element where the game blocks will be created
+		var all_blocks = document.querySelector("div.steam-all-games");
+
+		//Read the list of games that should appear in each block
+		for (var i = 0; i < steam_games.length; i++) {
+		if (!fs.existsSync('/tmp/regataos-gcs/config/steam-games/json/installed/' + steam_games[i].appid + '.json')) {
+
+			//Request the creation of the new element (block) for each game
+			var steam_game_blocks = document.createElement("div");
+
+			//Add classes to the new game blocks
+			steam_game_blocks.classList.add("app-block-steam");
+
+			//Add the game image in the background
+			steam_game_blocks.style.backgroundImage = "url('./../images/games-backg/steam/steam.jpg')";
+
+			//Add game details within the newly created block
+			steam_game_blocks.innerHTML = ' \
+			<div class="steam-game-img" style="background-image: url(https://cdn.cloudflare.steamstatic.com/steam/apps/'+ steam_games[i].appid + '/header.jpg)"></div> \
+			<div class="block-play-steam"> \
+				<div id="' + steam_games[i].appid + '" class="install-box-steam" onclick="window.gameid=this.id; run_steam_game();"> \
+					<div class="play-button"> \
+						<i class="fas fa-download"></i><div class="install-txt">Instalar</div> \
+					</div> \
+				</div> \
+			</div> \
+			<div class="block-text-steam" title="' + steam_games[i].name +'"> \
+				<div class="block-title">' + steam_games[i].name + '</div> \
+				<div class="block-desc">Steam</div> \
+			</div>';
+
+			//Finally, create the new game blocks dynamically
+			all_blocks.appendChild(steam_game_blocks);
+		}
+		}
+		return;
+	}
+	});
+	});
+}
+list_steam_games();
+
+// This script helps to dynamically create game blocks
 function games_installed_steam() {
 	var fs = require("fs");
 
 	var files = [];
 
 	// Read JSON files with the list of games
-	fs.readdirSync("/tmp/regataos-gcs/config/steam-games/json").forEach(files => {
-	fs.readFile("/tmp/regataos-gcs/config/steam-games/json/" +files , "utf8", function(err, data) {
+	fs.readdirSync("/tmp/regataos-gcs/config/steam-games/json/installed").forEach(files => {
+	fs.readFile("/tmp/regataos-gcs/config/steam-games/json/installed/" +files , "utf8", function(err, data) {
 	if(!err) {
 	var games = JSON.parse(data);
 
@@ -27,7 +83,7 @@ function games_installed_steam() {
 
 			//Add game details within the newly created block
 			new_game_blocks.innerHTML = ' \
-			<div class="steam-game-img" style="background-image: url(' + games[i].image + ')"></div> \
+			<div class="steam-game-img" style="background-image: url(https://cdn.cloudflare.steamstatic.com/steam/apps/'+ games[i].appid + '/header.jpg)"></div> \
 			<div class="block-play-steam"> \
 				<div id="' + games[i].appid + '" class="play-box-steam" onclick="window.gameid=this.id; run_steam_game();"> \
 					<div class="play-button"> \
@@ -37,7 +93,7 @@ function games_installed_steam() {
 			</div> \
 			<div class="block-text-steam" title="' + games[i].name +'"> \
 				<div class="block-title">' + games[i].name + '</div> \
-				<div class="block-desc">' + games[i].launcher + '</div> \
+				<div class="block-desc">Steam</div> \
 			</div>';
 
 			//Finally, create the new game blocks dynamically
@@ -48,5 +104,4 @@ function games_installed_steam() {
 	});
 	});
 }
-
 games_installed_steam();
