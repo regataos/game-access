1ff9a6948c569c7d4fef11fb0dec48e8057d90b8
josue2363 <79342913+josue2363@users.noreply.github.com>
2021-09-08 20:46:40 -0300

    Update blocks_functions.js

--- blocks_functions.js
+++ blocks_functions.js
@@ -46,6 +46,24 @@ function block_function3() {
 	const fs = require('fs');
 
 	var installed_launchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
+	if ((installed_launchers.indexOf("ubisoftconnect") > -1) == "1") {
+		var command_line = 'cd /opt/regataos-wine/desktop-files/; gtk-launch "Ubisoft Connect.desktop"';
+		exec(command_line,function(error,call,errlog){
+		});
+
+	} else {
+		var comando = 'echo "ubisoftconnect" > "/tmp/regataos-gcs/confirm-installation"';
+		console.log(comando);
+		exec(comando,function(error,call,errlog){
+		});
+	}
+}
+
+function block_function3_epic() {
+	const exec = require('child_process').exec;
+	const fs = require('fs');
+
+	var installed_launchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
 	if ((installed_launchers.indexOf("epicstore") > -1) == "1") {
 		var command_line = 'cd /opt/regataos-wine/desktop-files/; gtk-launch "Epic Games Launcher.desktop"';
 		exec(command_line,function(error,call,errlog){
