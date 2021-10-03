52d922370c85d5110fd54b32c9e3cb72d55ee5bc
josue2363 <79342913+josue2363@users.noreply.github.com>
2021-10-03 16:02:36 -0300

    Add files via upload

--- home-blocks-content.js
+++ home-blocks-content.js
@@ -1,128 +1,90 @@
 function home_content() {
-    const exec = require('child_process').exec;
+    $(document).ready(function() {
+        // Content of block 1
+        //Title
+        $(".appbc1 .block-title").text("Europa Universalis IV");
+        //Block image
+        $(".appbc1").css("background-image", 'url("file:///tmp/regataos-gcs/config/cache/img/block-img1.jpg")');
+        $(".appbc1").css("background-position", "50% 50%")
+
+        // Content of block 2
+        //Title
+        $(".appbc2 .block-title").text("Path of Exile");
+        //Block image
+        $(".appbc2").css("background-image", 'url("file:///tmp/regataos-gcs/config/cache/img/block-img2.jpg")');
+        $(".appbc2").css("background-position", "50% 0%")
+
+        // Content of block 3
+        //Title
+        $(".appbc3 .block-title").text("Rocket League");
+        //Block image
+        $(".appbc3").css("background-image", 'url("file:///tmp/regataos-gcs/config/cache/img/block-img3.jpg")');
+        $(".appbc3").css("background-position", "50% 0%")
+    });
 
     // Detect User Language
     var command_line = "echo $LANG";
+    const exec = require('child_process').exec;
     exec(command_line, (error, stdout, stderr) => {
     if (stdout) {
         var user_language = stdout
         if ((user_language.indexOf("pt_BR") > -1) == "1") {
             $(document).ready(function() {
                 // Content of block 1
-                //Block image
-                $(".appbc1").css("background-image", 'url("https://cdn1.epicgames.com/salesEvent/salesEvent/3bc192d2-c308-49ff-b381-a4833b954cec_2560x1440-5b2cec10a59f7594e863eff0a51ecc37?h=480&resize=1&w=854")');
-                $(".appbc1").css("background-position", "50% 50%")
-
-                //Title and description
-                $(".appbc1 .block-title").text("Speed Brawl");
-                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 23 de setembro.");
+                //Description
+                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 07 de outubro.");
 
                 // Content of block 2
-                //Block image
-                $(".appbc2").css("background-image", 'url("https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_Tharsis_ChoiceProvisions_S1_2560x1440-f2bafbbf1629a48af6e394261b2dc3e7?h=480&amp;resize=1&amp;w=854")');
-                $(".appbc2").css("background-position", "50% 0%")
-
-                //Title and description
-                $(".appbc2 .block-title").text("Tharsis");
-                $(".appbc2 .block-desc").text("Disponível de graça na Epic Games até 23 de setembro.");
+                //Description
+                $(".appbc2 .block-desc").text("Disponível de graça na Epic Games Store.");
 
                 // Content of block 3
-                //Block image
-                $(".appbc3").css("background-image", 'url("https://i.ibb.co/bPzDBvg/block2.jpg")');
-                $(".appbc3").css("background-position", "50% 0%")
-
-                //Title and description
-                $(".appbc3 .block-title").text("Rocket League");
+                //Description
                 $(".appbc3 .block-desc").text("Disponível de graça na Epic Games Store.");
             });
         
         } else if ((user_language.indexOf("pt_PT") > -1) == "1") {
             $(document).ready(function() {
                 // Content of block 1
-                //Block image
-                $(".appbc1").css("background-image", 'url("https://cdn1.epicgames.com/salesEvent/salesEvent/3bc192d2-c308-49ff-b381-a4833b954cec_2560x1440-5b2cec10a59f7594e863eff0a51ecc37?h=480&resize=1&w=854")');
-                $(".appbc1").css("background-position", "50% 50%")
-
-                //Title and description
-                $(".appbc1 .block-title").text("Speed Brawl");
-                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 23 de setembro.");
+                //Description
+                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 07 de outubro.");
 
                 // Content of block 2
-                //Block image
-                $(".appbc2").css("background-image", 'url("https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_Tharsis_ChoiceProvisions_S1_2560x1440-f2bafbbf1629a48af6e394261b2dc3e7?h=480&amp;resize=1&amp;w=854")');
-                $(".appbc2").css("background-position", "50% 0%")
-
-                //Title and description
-                $(".appbc2 .block-title").text("Tharsis");
-                $(".appbc2 .block-desc").text("Disponível de graça na Epic Games até 23 de setembro.");
+                //Description
+                $(".appbc2 .block-desc").text("Disponível de graça na Epic Games Store.");
 
                 // Content of block 3
-                //Block image
-                $(".appbc3").css("background-image", 'url("https://i.ibb.co/bPzDBvg/block2.jpg")');
-                $(".appbc3").css("background-position", "50% 0%")
-
-                //Title and description
-                $(".appbc3 .block-title").text("Rocket League");
+                //Description
                 $(".appbc3 .block-desc").text("Disponível de graça na Epic Games Store.");
             });
 
         } else if ((user_language.indexOf("en_US") > -1) == "1") {
             $(document).ready(function() {
                 // Content of block 1
-                //Block image
-                $(".appbc1").css("background-image", 'url("https://cdn1.epicgames.com/salesEvent/salesEvent/3bc192d2-c308-49ff-b381-a4833b954cec_2560x1440-5b2cec10a59f7594e863eff0a51ecc37?h=480&resize=1&w=854")');
-                $(".appbc1").css("background-position", "50% 50%")
-
-                //Title and description
-                $(".appbc1 .block-title").text("Speed Brawl");
-                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until September 23.");
+                //Description
+                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until October 07.");
 
                 // Content of block 2
-                //Block image
-                $(".appbc2").css("background-image", 'url("https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_Tharsis_ChoiceProvisions_S1_2560x1440-f2bafbbf1629a48af6e394261b2dc3e7?h=480&amp;resize=1&amp;w=854")');
-                $(".appbc2").css("background-position", "50% 0%")
-
-                //Title and description
-                $(".appbc2 .block-title").text("Tharsis");
-                $(".appbc2 .block-desc").text("Available for free at the Epic Games until September 23.");
+                //Description
+                $(".appbc2 .block-desc").text("Available for free on the Epic Games Store.");
 
                 // Content of block 3
-                //Block image
-                $(".appbc3").css("background-image", 'url("https://i.ibb.co/bPzDBvg/block2.jpg")');
-                $(".appbc3").css("background-position", "50% 0%")
-
-                //Title and description
-                $(".appbc3 .block-title").text("Rocket League");
+                //Description
                 $(".appbc3 .block-desc").text("Available for free on the Epic Games Store.");
             });
 
         } else {
             $(document).ready(function() {
                 // Content of block 1
-                //Block image
-                $(".appbc1").css("background-image", 'url("https://cdn1.epicgames.com/salesEvent/salesEvent/3bc192d2-c308-49ff-b381-a4833b954cec_2560x1440-5b2cec10a59f7594e863eff0a51ecc37?h=480&resize=1&w=854")');
-                $(".appbc1").css("background-position", "50% 50%")
-
-                //Title and description
-                $(".appbc1 .block-title").text("Speed Brawl");
-                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until September 23.");
+                //Description
+                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until October 07.");
 
                 // Content of block 2
-                //Block image
-                $(".appbc2").css("background-image", 'url("https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_Tharsis_ChoiceProvisions_S1_2560x1440-f2bafbbf1629a48af6e394261b2dc3e7?h=480&amp;resize=1&amp;w=854")');
-                $(".appbc2").css("background-position", "50% 0%")
-
-                //Title and description
-                $(".appbc2 .block-title").text("Tharsis");
-                $(".appbc2 .block-desc").text("Available for free at the Epic Games until September 23.");
+                //Description
+                $(".appbc2 .block-desc").text("Available for free on the Epic Games Store.");
 
                 // Content of block 3
-                //Block image
-                $(".appbc3").css("background-image", 'url("https://i.ibb.co/bPzDBvg/block2.jpg")');
-                $(".appbc3").css("background-position", "50% 0%")
-
-                //Title and description
-                $(".appbc3 .block-title").text("Rocket League");
+                //Description
                 $(".appbc3 .block-desc").text("Available for free on the Epic Games Store.");
             });
         }
