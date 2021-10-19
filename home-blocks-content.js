60a9656cd9cfaf4f7c92286a7b36ff1fdb5913c7
josue2363 <79342913+josue2363@users.noreply.github.com>
2021-10-19 13:57:07 -0300

    Update home-blocks-content.js

--- home-blocks-content.js
+++ home-blocks-content.js
@@ -2,7 +2,7 @@ function home_content() {
     $(document).ready(function() {
         // Content of block 1
         //Title
-        $(".appbc1 .block-title").text("PC Building Simulator");
+        $(".appbc1 .block-title").text("Stubbs the Zombie in Rebel Without a Pulse");
         //Block image
         $(".appbc1").css("background-image", 'url("file:///tmp/regataos-gcs/config/cache/img/block-img1.jpg")');
         $(".appbc1").css("background-position", "50% 50%")
@@ -32,7 +32,7 @@ function home_content() {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 14 de outubro.");
+                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 21 de outubro.");
 
                 // Content of block 2
                 //Description
@@ -47,7 +47,7 @@ function home_content() {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 14 de outubro.");
+                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 21 de outubro.");
 
                 // Content of block 2
                 //Description
@@ -62,7 +62,7 @@ function home_content() {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until October 14.");
+                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until October 21.");
 
                 // Content of block 2
                 //Description
@@ -77,7 +77,7 @@ function home_content() {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until October 14.");
+                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until October 21.");
 
                 // Content of block 2
                 //Description
