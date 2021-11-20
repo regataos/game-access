a5e170659ec777ee347ca3554f53e5cc08614258
josue2363 <79342913+josue2363@users.noreply.github.com>
2021-11-19 22:21:51 -0300

    Update home-blocks-content.js

--- home-blocks-content.js
+++ home-blocks-content.js
@@ -2,7 +2,7 @@ function home_content() {
     $(document).ready(function() {
         // Content of block 1
         //Title
-        $(".appbc1 .block-title").text("Aven Colony");
+        $(".appbc1 .block-title").text("Never Alone (Kisima Ingitchuna)");
         //Block image
         $(".appbc1").css("background-image", 'url("file:///tmp/regataos-gcs/config/cache/img/block-img1.jpg")');
         $(".appbc1").css("background-position", "50% 50%")
@@ -32,7 +32,7 @@ function home_content() {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 11 de novembro.");
+                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 25 de novembro.");
 
                 // Content of block 2
                 //Description
@@ -47,7 +47,7 @@ function home_content() {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 11 de novembro.");
+                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até 25 de novembro.");
 
                 // Content of block 2
                 //Description
@@ -62,7 +62,7 @@ function home_content() {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until November 11.");
+                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until November 25.");
 
                 // Content of block 2
                 //Description
@@ -77,7 +77,7 @@ function home_content() {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until November 11.");
+                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until November 25.");
 
                 // Content of block 2
                 //Description
