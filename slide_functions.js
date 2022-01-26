1f83f240ed5593e1c837bbfc69f1b4ece09f2df0
josue2363 <79342913+josue2363@users.noreply.github.com>
2022-01-26 11:18:54 -0300

    Update slide_functions.js

--- slide_functions.js
+++ slide_functions.js
@@ -90,31 +90,22 @@ if ((installed_launchers.indexOf("origin") > -1) == "1") {
 }
 
 function slide_function4() {
-const exec = require('child_process').exec;
-const fs = require('fs');
+	const exec = require('child_process').exec;
+	const fs = require('fs');
 
-var installed_launchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
-if ((installed_launchers.indexOf("origin") > -1) == "1") {
-
-	var installed_games = fs.readFileSync("/tmp/regataos-gcs/config/installed-games.conf", "utf8");
-	if ((installed_games.indexOf("anthem") > -1) == "1") {
-
-		var command_line = 'export GAMEVAR="WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1"; export GAME="anthem"; export LAUNCHER="origin"; export RUNGAME=""; /opt/regataos-gcs/scripts/action-games/rungame';
-		console.log(command_line);
+	var installed_launchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
+	if ((installed_launchers.indexOf("epicstore") > -1) == "1") {
+		var command_line = 'cd /opt/regataos-wine/desktop-files/; gtk-launch "Epic Games Launcher.desktop"';
 		exec(command_line,function(error,call,errlog){
 		});
 
 	} else {
-		var command_line = 'export LAUNCHERVAR="WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1"; export LAUNCHER="origin"; export RUNLAUNCHER="dosdevices/c:/Program Files (x86)/Origin/Origin.exe"; /opt/regataos-gcs/scripts/action-games/runlauncher';
-		console.log(command_line);
-		exec(command_line,function(error,call,errlog){
-		});
-	}
+		if (fs.existsSync("/tmp/regataos-gcs/config/epicstore-games/show-egs.txt")) {
+			window.open("https://www.epicgames.com/store/p/control", 'popup')
 
-} else {
-	var comando = 'echo "origin" > "/tmp/regataos-gcs/confirm-installation"';
-	console.log(comando);
-	exec(comando,function(error,call,errlog){
-	});
-}
+		} else {
+			url = "./../pages/epicstore-games.html";
+			window.location.assign(url)
+		}
+	}
 }
