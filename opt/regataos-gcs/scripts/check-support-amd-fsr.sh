5b4580e2985e1c20c794e86a56167fc7b282e59a
Regata OS Project <regataosproject@gmail.com>
2022-02-10 10:50:02 -0300

    Fix for GPU detection to enable AMD FSR.

--- opt/regataos-gcs/scripts/check-support-amd-fsr.sh
+++ opt/regataos-gcs/scripts/check-support-amd-fsr.sh
@@ -3,29 +3,18 @@
 # This script checks whether AMD FidelityFX Super Resolution​ (FSR)
 # should be enabled or disabled by checking user settings or hardware.
 
-# AMD FSR settings have not yet been decided by the user, checking the
-# need to enable the feature based on the hardware of the machine.
-function check_hardware() {
-	vulkan_test=$(vulkaninfo)
-	if [[ $vulkan_test == *"Instance Extensions"* ]]; then
-		if [[ $vulkan_test != *"Vulkan support is incomplete"* ]]; then
-			if test ! -e "/tmp/regataos-prime/use-hybrid-graphics.txt"; then
-				if [[ $(inxi -G | egrep -i "Card-1|Device-1" | cut -d":" -f 3- | cut -d"[" -f 2- | cut -d"]" -f -1) == *"Radeon Vega"* ]]; then
-					vulkan_support="vulkan_supported"
-				fi
-			fi
-		fi
-	fi
-}
-
 # Check User Settings for AMD FSR
 configuration_status=$(grep -r "amd-fsr=" $HOME/.config/regataos-gcs/regataos-gcs.conf)
 if [ -z $configuration_status ]; then
-	check_hardware
-
-	if [[ $(echo $vulkan_support) == *"vulkan_supported"* ]]; then
+	# AMD FSR settings have not yet been decided by the user, checking the
+	# need to enable the feature based on the hardware of the machine.
+	check_gpu=$(cat $HOME/.config/regataos-prime/system-info/dgpu-model.txt)
+	if [[ $(echo "$check_gpu") == *"Radeon Vega"* ]]; then
+		echo "GPU: $check_gpu | AMD FSR: on"
 		echo "amd-fsr=true" >> "$HOME/.config/regataos-gcs/regataos-gcs.conf"
 	else
+		echo "$check_gpu"
+		echo "GPU: $check_gpu | AMD FSR: off"
 		echo "amd-fsr=false" >> "$HOME/.config/regataos-gcs/regataos-gcs.conf"
 	fi
 fi
