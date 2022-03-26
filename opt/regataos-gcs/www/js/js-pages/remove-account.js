aa252c47d03f041564c09c3e4ad45d70b59f4557
Regata OS Project <regataosproject@gmail.com>
2022-03-26 19:24:04 -0300

    Uninstall Epic software when removing Epic Games Store account.

--- opt/regataos-gcs/www/js/js-pages/remove-account.js
+++ opt/regataos-gcs/www/js/js-pages/remove-account.js
@@ -25,6 +25,7 @@ const exec = require('child_process').exec;
 		var command_line = 'rm -f "/tmp/regataos-gcs/remove-user-account-epicstore.txt"; \
 		rm -f "/tmp/regataos-gcs/config/epicstore-games/show-egs.txt"; \
 		rm -rf "$HOME/.local/share/wineprefixes/epicstore-compatibility-mode"\
+		/opt/regataos-gcs/scripts/remove/scripts-remove/epicstore-compatibility-mode-rm.sh start"\
 		/opt/regataos-gcs/tools/legendary/legendary auth --delete';
 		console.log(command_line);
 		exec(command_line,function(error,call,errlog){
