c884bc73ec1cb0c3fa12c54e83e52a157189c193
Regata OS Project <regataosproject@gmail.com>
2022-03-26 19:21:00 -0300

    Delete wineprefix when removing Epic Games Store account.

--- opt/regataos-gcs/www/js/js-pages/remove-account.js
+++ opt/regataos-gcs/www/js/js-pages/remove-account.js
@@ -24,6 +24,7 @@ const exec = require('child_process').exec;
 	setTimeout(function(){
 		var command_line = 'rm -f "/tmp/regataos-gcs/remove-user-account-epicstore.txt"; \
 		rm -f "/tmp/regataos-gcs/config/epicstore-games/show-egs.txt"; \
+		rm -rf "$HOME/.local/share/wineprefixes/epicstore-compatibility-mode"\
 		/opt/regataos-gcs/tools/legendary/legendary auth --delete';
 		console.log(command_line);
 		exec(command_line,function(error,call,errlog){
