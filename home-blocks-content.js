48e5e471ac05677549a76b2a644e4b9cb443911d
josue2363 <79342913+josue2363@users.noreply.github.com>
2022-09-10 21:43:54 -0300

    Update home-blocks-content.js

--- home-blocks-content.js
+++ home-blocks-content.js
@@ -2,13 +2,13 @@ function home_content() {
     $(document).ready(function() {
         // Content of block 1
         //Title
-        $(".appbc1 .block-title").text("Shadow of the Tomb Raider: DE");
+        $(".appbc1 .block-title").text("Hundred Days");
         //Block image
         $(".appbc1").css("background-image", 'url("file:///tmp/regataos-gcs/config/cache/img/block-img1.webp")');
         $(".appbc1").css("background-position", "50% 50%")
         //Date
-        date_pt_br="08 de setembro"
-        date_en_us="September 08"
+        date_pt_br="15 de setembro"
+        date_en_us="September 15"
 
         // Content of block 2
         //Title
