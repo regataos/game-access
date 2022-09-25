f285d54bcca8e66a1a8b72629e660836952d6545
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2022-09-25 14:29:43 -0300

    Update home-blocks-content.js

--- home-blocks-content.js
+++ home-blocks-content.js
@@ -2,13 +2,13 @@ function home_content() {
     $(document).ready(function() {
         // Content of block 1
         //Title
-        $(".appbc1 .block-title").text("Spirit of the North");
+        $(".appbc1 .block-title").text("ARK: Survival Evolved");
         //Block image
         $(".appbc1").css("background-image", 'url("file:///tmp/regataos-gcs/config/cache/img/block-img1.webp")');
         $(".appbc1").css("background-position", "50% 50%")
         //Date
-        date_pt_br="22 de setembro"
-        date_en_us="September 22"
+        date_pt_br="29 de setembro"
+        date_en_us="September 29"
 
         // Content of block 2
         //Title
