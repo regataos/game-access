379db9d6d7e1c0c1d760ee8df4711bdfc5fd6d8b
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2022-11-25 15:09:08 -0300

    Update slide_functions.js

--- slide_functions.js
+++ slide_functions.js
@@ -11,10 +11,10 @@ const exec = require('child_process').exec;
 const fs = require('fs');
 
 var installed_launchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
-if ((installed_launchers.indexOf("battlenet") > -1) == "1") {
+if ((installed_launchers.indexOf("origin") > -1) == "1") {
 
 	var installed_games = fs.readFileSync("/tmp/regataos-gcs/config/installed-games.conf", "utf8");
-	if ((installed_games.indexOf("overwatch") > -1) == "1") {
+	if ((installed_games.indexOf("ts4") > -1) == "1") {
 
 		var command_line = 'export GAMEVAR="WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1"; export GAME="overwatch"; export LAUNCHER="battlenet"; export RUNGAME=""; /opt/regataos-gcs/scripts/action-games/rungame';
 		console.log(command_line);
@@ -22,14 +22,14 @@ if ((installed_launchers.indexOf("battlenet") > -1) == "1") {
 		});
 
 	} else {
-		var command_line = 'export LAUNCHERVAR="WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1"; export LAUNCHER="battlenet"; export RUNLAUNCHER="dosdevices/c:/Program Files (x86)/Battle.net/Battle.net.exe"; /opt/regataos-gcs/scripts/action-games/runlauncher';
+		var command_line = 'export LAUNCHERVAR="WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1"; export LAUNCHER="origin"; export RUNLAUNCHER="dosdevices/c:/Program Files (x86)/Origin/Origin.exe"; /opt/regataos-gcs/scripts/action-games/runlauncher';
 		console.log(command_line);
 		exec(command_line,function(error,call,errlog){
 		});
 	}
 
 } else {
-	var comando = 'echo "battlenet" > "/tmp/regataos-gcs/confirm-installation"';
+	var comando = 'echo "origin" > "/tmp/regataos-gcs/confirm-installation"';
 	console.log(comando);
 	exec(comando,function(error,call,errlog){
 	});
