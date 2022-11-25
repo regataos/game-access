ba29bd4ae42f78cbc62463812c9316e8f59c43b2
Regata OS Project <regataosproject@gmail.com>
2022-11-25 15:37:56 -0300

    Update slide_functions.js file.

--- slide_functions.js
+++ slide_functions.js
@@ -1,79 +1,80 @@
 // Stilde functions
 function slide_function1() {
-	setTimeout(function(){
-        	sessionStorage.setItem("game-gcs-id", "lol");
-        	window.location.href = './../pages/gcs-games.html';
+	setTimeout(function () {
+		sessionStorage.setItem("game-gcs-id", "lol");
+		window.location.href = './../pages/gcs-games.html';
 	}, 500);
 }
 
 function slide_function2() {
-const exec = require('child_process').exec;
-const fs = require('fs');
+	const exec = require('child_process').exec;
+	const fs = require('fs');
 
-var installed_launchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
-if ((installed_launchers.indexOf("origin") > -1) == "1") {
+	const listInstalledLaunchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
+	const listInstalledGames = fs.readFileSync("/tmp/regataos-gcs/config/installed-games.conf", "utf8");
 
-	var installed_games = fs.readFileSync("/tmp/regataos-gcs/config/installed-games.conf", "utf8");
-	if ((installed_games.indexOf("ts4") > -1) == "1") {
+	if ((listInstalledLaunchers.indexOf("origin") > -1) == "1") {
+		if ((listInstalledGames.indexOf("ts4") > -1) == "1") {
+			const runGame = `
+			export GAME="ts4";
+			export LAUNCHER="origin";
+			export RUNGAME="origin://LaunchGame/OFB-EAST:109552299";
+			/opt/regataos-gcs/scripts/action-games/rungame`;
+			exec(runGame, function (error, call, errlog) {
+			});
 
-		var command_line = 'export GAMEVAR="WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1"; export GAME="overwatch"; export LAUNCHER="battlenet"; export RUNGAME=""; /opt/regataos-gcs/scripts/action-games/rungame';
-		console.log(command_line);
-		exec(command_line,function(error,call,errlog){
-		});
+		} else {
+			const runLauncher = 'cd /opt/regataos-wine/desktop-files/; gtk-launch "Origin.desktop"';
+			exec(runLauncher, function (error, call, errlog) {
+			});
+		}
 
 	} else {
-		var command_line = 'export LAUNCHERVAR="WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1"; export LAUNCHER="origin"; export RUNLAUNCHER="dosdevices/c:/Program Files (x86)/Origin/Origin.exe"; /opt/regataos-gcs/scripts/action-games/runlauncher';
-		console.log(command_line);
-		exec(command_line,function(error,call,errlog){
+		const installLancher = 'echo "origin" > "/tmp/regataos-gcs/confirm-installation"';
+		exec(installLancher, function (error, call, errlog) {
 		});
 	}
-
-} else {
-	var comando = 'echo "origin" > "/tmp/regataos-gcs/confirm-installation"';
-	console.log(comando);
-	exec(comando,function(error,call,errlog){
-	});
-}
 }
 
 function slide_function3() {
-const exec = require('child_process').exec;
-const fs = require('fs');
+	const exec = require('child_process').exec;
+	const fs = require('fs');
 
-var installed_launchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
-if ((installed_launchers.indexOf("origin") > -1) == "1") {
+	const listInstalledLaunchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
+	const listInstalledGames = fs.readFileSync("/tmp/regataos-gcs/config/installed-games.conf", "utf8");
 
-	var installed_games = fs.readFileSync("/tmp/regataos-gcs/config/installed-games.conf", "utf8");
-	if ((installed_games.indexOf("bfv") > -1) == "1") {
+	if ((listInstalledLaunchers.indexOf("origin") > -1) == "1") {
+		if ((listInstalledGames.indexOf("bfv") > -1) == "1") {
+			const runGame = `
+			export GAME="bfv";
+			export LAUNCHER="origin";
+			export RUNGAME="origin://LaunchGame/Origin.OFR.50.0004662";
+			/opt/regataos-gcs/scripts/action-games/rungame`;
+			exec(runGame, function (error, call, errlog) {
+			});
 
-		var command_line = 'export GAMEVAR="WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1"; export GAME="bfv"; export LAUNCHER="origin"; export RUNGAME=""; /opt/regataos-gcs/scripts/action-games/rungame';
-		console.log(command_line);
-		exec(command_line,function(error,call,errlog){
-		});
+		} else {
+			const runLauncher = 'cd /opt/regataos-wine/desktop-files/; gtk-launch "Origin.desktop"';
+			exec(runLauncher, function (error, call, errlog) {
+			});
+		}
 
 	} else {
-		var command_line = 'export LAUNCHERVAR="WINEESYNC=1 WINE_LARGE_ADDRESS_AWARE=1"; export LAUNCHER="origin"; export RUNLAUNCHER="dosdevices/c:/Program Files (x86)/Origin/Origin.exe"; /opt/regataos-gcs/scripts/action-games/runlauncher';
-		console.log(command_line);
-		exec(command_line,function(error,call,errlog){
+		const installLancher = 'echo "origin" > "/tmp/regataos-gcs/confirm-installation"';
+		exec(installLancher, function (error, call, errlog) {
 		});
 	}
-
-} else {
-	var comando = 'echo "origin" > "/tmp/regataos-gcs/confirm-installation"';
-	console.log(comando);
-	exec(comando,function(error,call,errlog){
-	});
-}
 }
 
 function slide_function4() {
 	const exec = require('child_process').exec;
 	const fs = require('fs');
 
-	var installed_launchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
-	if ((installed_launchers.indexOf("epicstore") > -1) == "1") {
-		var command_line = 'cd /opt/regataos-wine/desktop-files/; gtk-launch "Epic Games Launcher.desktop"';
-		exec(command_line,function(error,call,errlog){
+	const listInstalledLaunchers = fs.readFileSync("/tmp/regataos-gcs/config/installed-launchers.conf", "utf8");
+
+	if ((listInstalledLaunchers.indexOf("epicstore") > -1) == "1") {
+		const runLauncher = 'cd /opt/regataos-wine/desktop-files/; gtk-launch "Epic Games Launcher.desktop"';
+		exec(runLauncher, function (error, call, errlog) {
 		});
 
 	} else {
