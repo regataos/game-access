24439bd7b0047eb21719efc28d15403014c41fb3
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2022-11-27 15:33:59 -0300

    Add files via upload

--- home-slide-content.js
+++ home-slide-content.js
@@ -1,5 +1,5 @@
 function home_content() {
-    $(document).ready(function() {
+    $(document).ready(function () {
         // Content of slide 1
         //Block image
         $(".slideshow .slide .slide-img1").css("background-image", 'url("file:///tmp/regataos-gcs/config/cache/img/slide-img1.jpg")');
@@ -23,107 +23,108 @@ function home_content() {
     });
 
     // Detect User Language
-    var command_line = "echo $LANG";
+    const checkLang = "echo $LANG";
     const exec = require('child_process').exec;
-    exec(command_line, (error, stdout, stderr) => {
-    if (stdout) {
-        var user_language = stdout
-        if ((user_language.indexOf("pt_BR") > -1) == "1") {
-            $(document).ready(function() {
-                // Content of slide 1
-                //Title and description
-                $(".slide-content .slide-desc1 p").text("Chame os amigos, escolha um Campeão e faça jogadas memoráveis");
-                $(".slide-content .slide-button1").text("Jogue agora");
+    exec(checkLang, (error, stdout, stderr) => {
+        if (stdout) {
+            const userLanguage = stdout
+            if ((userLanguage.indexOf("pt_BR") > -1) == "1") {
+                $(document).ready(function () {
+                    // Content of slide 1
+                    //Title and description
+                    $(".slide-content .slide-desc1 p").text("Chame os amigos, escolha um Campeão e faça jogadas memoráveis");
+                    $(".slide-content .slide-button1").text("Jogue agora");
 
-                // Content of slide 2
-                //Title and description
-                $(".slide-content .slide-desc2 p").text("Solte sua imaginação e dê aos seus Sims aparências e personalidades únicas");
-                $(".slide-content .slide-button2").text("Jogue agora");
+                    // Content of slide 2
+                    //Title and description
+                    $(".slide-content .slide-desc2 p").text("Solte sua imaginação e dê aos seus Sims aparências e personalidades únicas");
+                    $(".slide-content .slide-button2").text("Jogue agora com Origin");
 
-                // Content of slide 3
-                //Title and description
-                $(".slide-content .slide-desc3 p").text("Prove seu valor enfrentando batalhas da Segunda Guerra Mundial");
-                $(".slide-content .slide-button3").text("Jogue agora com Origin");
+                    // Content of slide 3
+                    //Title and description
+                    $(".slide-content .slide-desc3 p").text("Prove seu valor enfrentando batalhas da Segunda Guerra Mundial");
+                    $(".slide-content .slide-button3").text("Jogue agora com Origin");
 
-                // Content of slide 4
-                //Title and description
-                $(".slide-content .slide-desc4 p").text("O mundo agora é sua arma em uma luta épica contra um inimigo assustador");
-                $(".slide-content .slide-button4").text("Jogue agora com a Epic Games Store");
-            });
+                    // Content of slide 4
+                    //Title and description
+                    $(".slide-content .slide-desc4 p").text("O mundo agora é sua arma em uma luta épica contra um inimigo assustador");
+                    $(".slide-content .slide-button4").text("Jogue agora com a Epic Games Store");
+                });
 
-        } else if ((user_language.indexOf("pt_PT") > -1) == "1") {
-            $(document).ready(function() {
-                // Content of slide 1
-                //Block image
-                $(".slideshow .slide .slide-img1").css("background-image", 'url("https://i.ibb.co/QK6SXCt/img1.jpg")');
+            } else if ((userLanguage.indexOf("pt_PT") > -1) == "1") {
+                $(document).ready(function () {
+                    // Content of slide 1
+                    //Block image
+                    $(".slideshow .slide .slide-img1").css("background-image", 'url("https://i.ibb.co/QK6SXCt/img1.jpg")');
 
-                //Title and description
-                $(".slide-content .slide-desc1 p").text("Chame os amigos, escolha um Campeão e faça jogadas memoráveis");
-                $(".slide-content .slide-button1").text("Jogue agora");
+                    //Title and description
+                    $(".slide-content .slide-desc1 p").text("Chame os amigos, escolha um Campeão e faça jogadas memoráveis");
+                    $(".slide-content .slide-button1").text("Jogue agora");
 
-                // Content of slide 2
-                //Title and description
-                $(".slide-content .slide-desc2 p").text("Solte sua imaginação e dê aos seus Sims aparências e personalidades únicas");
-                $(".slide-content .slide-button2").text("Jogue agora");
+                    // Content of slide 2
+                    //Title and description
+                    $(".slide-content .slide-desc2 p").text("Solte sua imaginação e dê aos seus Sims aparências e personalidades únicas");
+                    $(".slide-content .slide-button2").text("Jogue agora com Origin");
 
-                // Content of slide 3
-                //Title and description
-                $(".slide-content .slide-desc3 p").text("Prove seu valor enfrentando batalhas da Segunda Guerra Mundial");
-                $(".slide-content .slide-button3").text("Jogue agora com Origin");
+                    // Content of slide 3
+                    //Title and description
+                    $(".slide-content .slide-desc3 p").text("Prove seu valor enfrentando batalhas da Segunda Guerra Mundial");
+                    $(".slide-content .slide-button3").text("Jogue agora com Origin");
 
-                // Content of slide 4
-                //Title and description
-                $(".slide-content .slide-desc4 p").text("O mundo agora é sua arma em uma luta épica contra um inimigo assustador");
-                $(".slide-content .slide-button4").text("Jogue agora com a Epic Games Store");
-            });
+                    // Content of slide 4
+                    //Title and description
+                    $(".slide-content .slide-desc4 p").text("O mundo agora é sua arma em uma luta épica contra um inimigo assustador");
+                    $(".slide-content .slide-button4").text("Jogue agora com a Epic Games Store");
+                });
 
-        } else if ((user_language.indexOf("en_US") > -1) == "1") {
-            $(document).ready(function() {
-                // Content of slide 1
-                //Title and description
-                $(".slide-content .slide-desc1 p").text("Call your friends, choose a champion and make memorable plays");
-                $(".slide-content .slide-button1").text("Play now");
+            } else if ((userLanguage.indexOf("en_US") > -1) == "1") {
+                $(document).ready(function () {
+                    // Content of slide 1
+                    //Title and description
+                    $(".slide-content .slide-desc1 p").text("Call your friends, choose a champion and make memorable plays");
+                    $(".slide-content .slide-button1").text("Play now");
 
-                // Content of slide 2
-                //Title and description
-                $(".slide-content .slide-desc2 p").text("Unleash your imagination and give your Sims unique looks and personalities");
-                $(".slide-content .slide-button2").text("Play now");
+                    // Content of slide 2
+                    //Title and description
+                    $(".slide-content .slide-desc2 p").text("Unleash your imagination and give your Sims unique looks and personalities");
+                    $(".slide-content .slide-button2").text("Play now with Origin");
 
-                // Content of slide 3
-               //Title and description
-                $(".slide-content .slide-desc3 p").text("Prove your worth by fighting WWII battles");
-                $(".slide-content .slide-button3").text("Play now with Origin");
+                    // Content of slide 3
+                    //Title and description
+                    $(".slide-content .slide-desc3 p").text("Prove your worth by fighting WWII battles");
+                    $(".slide-content .slide-button3").text("Play now with Origin");
 
-                // Content of slide 4
-                //Title and description
-                $(".slide-content .slide-desc4 p").text("The world is now your weapon in an epic fight against a terrifying enemy");
-                $(".slide-content .slide-button4").text("Play now with the Epic Games Store");
-            });
+                    // Content of slide 4
+                    //Title and description
+                    $(".slide-content .slide-desc4 p").text("The world is now your weapon in an epic fight against a terrifying enemy");
+                    $(".slide-content .slide-button4").text("Play now with the Epic Games Store");
+                });
 
-        } else {
-            $(document).ready(function() {
-                // Content of slide 1
-                //Title and description
-                $(".slide-content .slide-desc1 p").text("Call your friends, choose a champion and make memorable plays");
-                $(".slide-content .slide-button1").text("Play now");
+            } else {
+                $(document).ready(function () {
+                    // Content of slide 1
+                    //Title and description
+                    $(".slide-content .slide-desc1 p").text("Call your friends, choose a champion and make memorable plays");
+                    $(".slide-content .slide-button1").text("Play now");
 
-                // Content of slide 2
-                //Title and description
-                $(".slide-content .slide-desc2 p").text("Unleash your imagination and give your Sims unique looks and personalities");
-                $(".slide-content .slide-button2").text("Play now");
+                    // Content of slide 2
+                    //Title and description
+                    $(".slide-content .slide-desc2 p").text("Unleash your imagination and give your Sims unique looks and personalities");
+                    $(".slide-content .slide-button2").text("Play now");
 
-                // Content of slide 3
-               //Title and description
-                $(".slide-content .slide-desc3 p").text("Prove your worth by fighting WWII battles");
-                $(".slide-content .slide-button3").text("Play now with Origin");
+                    // Content of slide 3
+                    //Title and description
+                    $(".slide-content .slide-desc3 p").text("Prove your worth by fighting WWII battles");
+                    $(".slide-content .slide-button3").text("Play now with Origin");
 
-                // Content of slide 4
-                //Title and description
-                $(".slide-content .slide-desc4 p").text("The world is now your weapon in an epic fight against a terrifying enemy");
-                $(".slide-content .slide-button4").text("Play now with the Epic Games Store");
-            });
+                    // Content of slide 4
+                    //Title and description
+                    $(".slide-content .slide-desc4 p").text("The world is now your weapon in an epic fight against a terrifying enemy");
+                    $(".slide-content .slide-button4").text("Play now with the Epic Games Store");
+                });
+            }
         }
-    }
     });
 }
+
 home_content();
