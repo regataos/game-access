f4d678207a0208227d913d7f10ac5986c5fae9d8
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2022-12-01 20:25:46 -0300

    Update update-cache-from-home.sh

--- update-cache-from-home.sh
+++ update-cache-from-home.sh
@@ -89,8 +89,8 @@ function downloadSlideImages() {
 function downloadBlockImages() {
 	#Image 1
 	function blockImage1() {
-		block1_image_name="block1-sws.webp"
-		block1_image_code="z6vgnbQ"
+		block1_image_name="block1-fth.webp"
+		block1_image_code="YjnJZTV"
 
 		if test ! -e "$HOME/.config/regataos-gcs/cache/img/$block1_image_name"; then
 			# Clear cache
