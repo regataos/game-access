4a648ef2360d24646a952c6849d0ec6fcef700e5
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2022-12-18 23:24:11 -0300

    Update home-blocks-online.js

--- home-blocks-online.js
+++ home-blocks-online.js
@@ -2,9 +2,9 @@ function home_content() {
     $(document).ready(function() {
         // Content of block 1
         //Title
-        $(".appbc1 .block-title").text("Saints Row IV: Re-Elected");
+        $(".appbc1 .block-title").text("Epic Games Store: Free Mystery Game");
         //Block image
-        $(".appbc1").css("background-image", 'url("https://i.ibb.co/6rFYmFw/block1-srv.webp")');
+        $(".appbc1").css("background-image", 'url("https://i.ibb.co/WcywJ0y/epic-block1.jpg")');
         $(".appbc1").css("background-position", "50% 50%")
         //Date
         date_pt_br="15 de dezembro"
@@ -21,28 +21,32 @@ function home_content() {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até " + date_pt_br + ".");
+                // $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até " + date_pt_br + ".");
+                $(".appbc1 .block-desc").text("Jogo misterioso grátis na Epic Games Store.");
             });
         
         } else if ((user_language.indexOf("pt_PT") > -1) == "1") {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até " + date_pt_br + ".");
+                // $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até " + date_pt_br + ".");
+                $(".appbc1 .block-desc").text("Jogo misterioso grátis na Epic Games Store.");
             });
 
         } else if ((user_language.indexOf("en_US") > -1) == "1") {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until " + date_en_us + ".");
+                // $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until " + date_en_us + ".");
+                $(".appbc1 .block-desc").text("Free Mysterious Game at Epic Games Store.");
             });
 
         } else {
             $(document).ready(function() {
                 // Content of block 1
                 //Description
-                $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until " + date_en_us + ".");
+                // $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until " + date_en_us + ".");
+                $(".appbc1 .block-desc").text("Free Mysterious Game at Epic Games Store.");
             });
         }
     }
