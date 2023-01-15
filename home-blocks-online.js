90dac035a8d0562caab7a27688f9f094cde37795
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2023-01-15 12:33:42 -0300

    Update home-blocks-online.js

--- home-blocks-online.js
+++ home-blocks-online.js
@@ -2,13 +2,13 @@ function home_content() {
     $(document).ready(function() {
         // Content of block 1
         //Title
-        $(".appbc1 .block-title").text("Kerbal Space Program");
+        $(".appbc1 .block-title").text("Divine Knockout (DKO)");
         //Block image
-        $(".appbc1").css("background-image", 'url("https://i.ibb.co/rMZzSHG/ksp-block1.webp")');
+        $(".appbc1").css("background-image", 'url("https://i.ibb.co/bbMWh6S/dko-block1.webp")');
         $(".appbc1").css("background-position", "0% 100%")
         //Date
-        date_pt_br="12 de janeiro"
-        date_en_us="January 12"
+        date_pt_br="19 de janeiro"
+        date_en_us="January 19"
     });
 
     // Detect User Language
