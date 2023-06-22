2b36dd7996e4dc55d5a7a7a36bc4f70d5d75412a
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2023-06-22 14:05:35 -0300

    Update home-blocks-online.js

--- home-blocks-online.js
+++ home-blocks-online.js
@@ -1,26 +1,50 @@
-function homeContent() {
-    // Content of block 1
-    const appbc1 = document.querySelector(".appbc1");
-    const blockTitle = appbc1.querySelector(".block-title");
-    const blockDesc = appbc1.querySelector(".block-desc");
-    
-    blockTitle.textContent = "theHunter: Call of the Wild";
-    appbc1.style.backgroundImage = 'url("https://i.ibb.co/tmStgh6/hcw-block1.webp")';
-    appbc1.style.backgroundPosition = "0% 100%";
-
-    const datePtBr = "29 de junho";
-    const dateEnUs = "June 29";
+function home_content() {
+    $(document).ready(function () {
+        // Content of block 1
+        //Title
+        $(".appbc1 .block-title").text("theHunter: Call of the Wild");
+        //Block image
+        $(".appbc1").css("background-image", 'url("https://i.ibb.co/tmStgh6/hcw-block1.webp")');
+        $(".appbc1").css("background-position", "0% 100%")
+        //Date
+        const date_pt_br = "29 de junho"
+        const date_en_us = "June 29"
+    });
 
     // Detect User Language
     const user_language = selectTranslation();
+    if ((user_language.indexOf("pt-br") > -1) == "1") {
+        $(document).ready(function () {
+            // Content of block 1
+            //Description
+            $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até " + date_pt_br + ".");
+            // $(".appbc1 .block-desc").text("Jogo misterioso grátis na Epic Games Store.");
+        });
 
-    if (user_language.includes("pt-br")) {
-        blockDesc.textContent = `Disponível de graça na Epic Games Store até ${datePtBr}.`;
-    } else if (user_language.includes("pt-pt")) {
-        blockDesc.textContent = `Disponível de graça na Epic Games Store até ${datePtBr}.`;
+    } else if ((user_language.indexOf("pt-pt") > -1) == "1") {
+        $(document).ready(function () {
+            // Content of block 1
+            //Description
+            $(".appbc1 .block-desc").text("Disponível de graça na Epic Games Store até " + date_pt_br + ".");
+            // $(".appbc1 .block-desc").text("Jogo misterioso grátis na Epic Games Store.");
+        });
+
+    } else if ((user_language.indexOf("en-us") > -1) == "1") {
+        $(document).ready(function () {
+            // Content of block 1
+            //Description
+            $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until " + date_en_us + ".");
+            // $(".appbc1 .block-desc").text("Free Mysterious Game at Epic Games Store.");
+        });
+
     } else {
-        blockDesc.textContent = `Available for free at the Epic Games Store until ${dateEnUs}.`;
+        $(document).ready(function () {
+            // Content of block 1
+            //Description
+            $(".appbc1 .block-desc").text("Available for free at the Epic Games Store until " + date_en_us + ".");
+            // $(".appbc1 .block-desc").text("Free Mysterious Game at Epic Games Store.");
+        });
     }
 }
 
-homeContent();
+home_content();
