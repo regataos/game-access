46d0e57c4d3ca7be76e71e672d453a3a22e3a207
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2023-07-08 11:37:24 -0300

    Update home-blocks-online.js

--- home-blocks-online.js
+++ home-blocks-online.js
@@ -2,13 +2,13 @@ function home_content() {
     $(document).ready(function () {
         // Content of block 1
         //Title
-        $(".appbc1 .block-title").text("theHunter: Call of the Wild");
+        $(".appbc1 .block-title").text("GRIME");
         //Block image
-        $(".appbc1").css("background-image", 'url("https://i.ibb.co/tmStgh6/hcw-block1.webp")');
+        $(".appbc1").css("background-image", 'url("https://cdn1.epicgames.com/spt-assets/2a14cec8a1fc4ab9a74dd46bf147c9e6/grime-p858i.jpg?h=480&quality=medium&resize=1&w=854")');
         $(".appbc1").css("background-position", "0% 100%")
         //Date
-        date_pt_br = "29 de junho"
-        date_en_us = "June 29"
+        date_pt_br = "13 de julho"
+        date_en_us = "July 13"
     });
 
     // Detect User Language
