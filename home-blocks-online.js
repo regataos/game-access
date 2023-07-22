41d3593ef4fdfa7833ec66707419cb9418a459db
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2023-07-22 14:28:00 -0300

    Update home-blocks-online.js

--- home-blocks-online.js
+++ home-blocks-online.js
@@ -2,13 +2,13 @@ function home_content() {
     $(document).ready(function () {
         // Content of block 1
         //Title
-        $(".appbc1 .block-title").text("Train Valley 2");
+        $(".appbc1 .block-title").text("The Elder Scrolls Online");
         //Block image
-        $(".appbc1").css("background-image", 'url("https://cdn1.epicgames.com/spt-assets/6d6a89f661f74d70bdc8be636c577056/train-valley-2-offer-bq0s1.jpg?h=480&quality=medium&resize=1&w=854")');
+        $(".appbc1").css("background-image", 'url("https://i.ibb.co/W2kYL76/teso-block1.webp")');
         $(".appbc1").css("background-position", "0% 100%")
         //Date
-        date_pt_br = "20 de julho"
-        date_en_us = "July 20"
+        date_pt_br = "27 de julho"
+        date_en_us = "July 27"
     });
 
     // Detect User Language
