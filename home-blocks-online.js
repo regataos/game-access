9ad3d9889b2c9de8f234c0e9c457a97ba68b0afc
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2023-11-25 13:11:38 -0300

    Update home-blocks-online.js

--- home-blocks-online.js
+++ home-blocks-online.js
@@ -1,3 +1,33 @@
+function showFreeGameHome() {
+    // Content of block 1
+    const gameTile = "Deliver Us Mars";
+    const gameImg = 'url("https://i.ibb.co/MBVMDp8/dm-block1.webp")';
+    const endPromotionPtBt = "30 de novembro";
+    const endPromotionEnUS = "November 30";
+
+    document.querySelector(".home-game1 .block-title").innerHTML = gameTile;
+    document.querySelector(".home-game1 .home-game-img").style.backgroundImage = gameImg;
+
+    // Detect User Language
+    const userLanguage = selectTranslation();
+
+    // Game block description 1
+    const gameBlockDesc1 = document.querySelector(".home-game1 .block-desc");
+
+    if (userLanguage.includes("pt-br") || userLanguage.includes("pt-pt")) {
+        // Game Description 1
+        gameBlockDesc1.innerHTML = `Disponível de graça na Epic Games Store até ${endPromotionPtBt}.`;
+    } else if (userLanguage.includes("en-us")) {
+        // Game Description 1
+        gameBlockDesc1.innerHTML = `Available for free at the Epic Games Store until ${endPromotionEnUS}.`;
+    } else {
+        // Game Description 1
+        gameBlockDesc1.innerHTML = `Available for free at the Epic Games Store until ${endPromotionEnUS}.`;
+    }
+}
+showFreeGameHome();
+
+// Keep old code for compatibility.
 function home_content() {
     $(document).ready(function () {
         // Content of block 1
@@ -46,5 +76,4 @@ function home_content() {
         });
     }
 }
-
 home_content();
