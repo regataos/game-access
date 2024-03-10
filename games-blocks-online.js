8a698f2f05928c5eeb3224f518d15bc3670250d9
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2024-03-10 00:29:04 -0300

    Update games-blocks-online.js

--- games-blocks-online.js
+++ games-blocks-online.js
@@ -1,9 +1,9 @@
 function showFreeGameHome() {
     // Content of block 1
-    const gameTile = "Super Meat Boy Forever";
-    const gameImg = 'url("https://i.ibb.co/VJH7xyY/smbf.webp")';
-    const endPromotionPtBt = "29 de fevereiro";
-    const endPromotionEnUS = "February 29";
+    const gameTile = "Astro Duel 2";
+    const gameImg = 'url("https://i.ibb.co/G7tK89H/astro-duel-2.webp")';
+    const endPromotionPtBt = "14 de fevereiro";
+    const endPromotionEnUS = "February 14";
 
     document.querySelector(".home-game1 .block-title").innerHTML = gameTile;
     document.querySelector(".home-game1 .home-game-img").style.backgroundImage = gameImg;
