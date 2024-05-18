68d95801a823a7b562869e288407a9d0e8df2f79
Josué Vicente <josue2363@gmail.com>
2024-05-17 21:01:26 -0300

    Switch to wine-gcs-8.0.

--- opt/regataos-gcs/scripts/install/en-us/download-wine-gcs.sh
+++ opt/regataos-gcs/scripts/install/en-us/download-wine-gcs.sh
@@ -3,7 +3,7 @@
 
 # Settings and variables
 app_name="Compatibility mode"
-app_download_link="https://github.com/regataos/wine-gcs/releases/download/wine-gcs-9.5/wine-gcs.tar.xz"
+app_download_link="https://github.com/regataos/wine-gcs/releases/download/wine-8.0/wine-gcs.tar.xz"
 app_download_file_name="wine-gcs.tar.xz"
 app_download_dir="/tmp/regataos-gcs"
 app_download_status="Downloading..."
