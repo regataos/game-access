f99244f76410375d82c9e6fa9038650422411a52
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2024-12-25 21:35:15 -0300

    Update games-blocks-online.js

--- games-blocks-online.js
+++ games-blocks-online.js
@@ -1,13 +1,9 @@
 function showFreeGameHome() {
     // Content of block 1
-    //const gameTile = "The Lord of The Rings Return to Moria";
     let gameTile = "";
     const gameImg = 'url("https://cdn1.epicgames.com/offer/d5241c76f178492ea1540fce45616757/EGS_HolidaySale_2024_WrappingPaper_1920x1080_1920x1080-a0f773a33796e48cc1d33e5072271fc8?resize=1&w=854&h=480&quality=medium")';
     const endPromotionPtBt = "19 de dezembro";
     const endPromotionEnUS = "December 19";
-
-    document.querySelector(".home-game1 .block-title").innerHTML = gameTile;
-    document.querySelector(".home-game1 .home-game-img").style.backgroundImage = gameImg;
 
     // Detect User Language
     const userLanguage = selectTranslation();
@@ -31,5 +27,8 @@ function showFreeGameHome() {
         gameTile = "Mysterious game on Epic Games Store";
         gameBlockDesc1.innerHTML = `Claim a mystery game for free on the Epic Games Store.`;
     }
+
+    document.querySelector(".home-game1 .block-title").innerHTML = gameTile;
+    document.querySelector(".home-game1 .home-game-img").style.backgroundImage = gameImg;
 }
 showFreeGameHome();
