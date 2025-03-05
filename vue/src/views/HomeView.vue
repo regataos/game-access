907e9af705ea2a7d786f99684a66d8ad340f352f
ToMattBan <souaquelecleyton@yandex.com>
2025-03-05 14:20:54 -0300

    Cleaning project

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>OI</h1>
   </main>
 </template>
+
+<script setup lang="ts">
+</script>
