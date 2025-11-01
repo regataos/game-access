3a5c548461d8753634fda00a6b0bf0fb0c9c1c44
Josué Vicente <79342913+josue2363@users.noreply.github.com>
2025-10-31 22:03:34 -0300

    Update games-blocks-online.js

--- games-blocks-online.js
+++ games-blocks-online.js
@@ -1,9 +1,9 @@
 function showFreeGameHome() {
     // Content of block 1
-    let gameTile = "Amnesia: The Bunker";
-    const gameImg = 'url("https://cdn1.epicgames.com/spt-assets/671aee4c8fb64a1c967d97f31f093e7a/amnesia-the-bunker-1x2n7.png?resize=1&w=854&h=480&quality=medium")';
-    const endPromotionPtBt = "23 de outubro";
-    const endPromotionEnUS = "october 23";
+    let gameTile = "Bendy and the Ink Machine";
+    const gameImg = 'url("https://i.ibb.co/G4CB7P5Q/bendy-and-the-ink-machine.webp")';
+    const endPromotionPtBt = "06 de novembro";
+    const endPromotionEnUS = "november 06";
 
     // Detect User Language
     const userLanguage = selectTranslation();
